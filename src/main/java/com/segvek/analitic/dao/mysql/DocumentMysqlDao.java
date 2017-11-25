b632e3b2d9cba7cdfdf4b786cc77fb4470fba25b
Panasenco Yurii <Panasenco@outlook.com>
2017-11-25 02:36:49 +0200

    database dump

--- src/main/java/com/segvek/analitic/dao/mysql/DocumentMysqlDao.java
+++ src/main/java/com/segvek/analitic/dao/mysql/DocumentMysqlDao.java
@@ -18,17 +18,17 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 @Service(value = "documentDAO")
-public class DocumentMysqlDao implements DocumentDAO{
+public class DocumentMysqlDao implements DocumentDAO {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
-    
+
     @Autowired
     BisnesRoleDAO bisnesRoleDAO;
-    
+
     @Autowired
     DocumentRowMapper documentRowMapper;
-    
+
     @Override
     public List<Document> getAllDocument() {
         String sql = "SELECT * FROM documents;";
@@ -68,52 +68,48 @@ public class DocumentMysqlDao implements DocumentDAO{
 
     @Override
     public Map<BisnesRole, String> getBisnesRolesByDocument(Document document) {
-        class MapElement{
+        class MapElement {
+
             BisnesRole br;
             String annotation;
+
             public MapElement(BisnesRole br, String annotation) {
                 this.br = br;
                 this.annotation = annotation;
             }
-            
         }
-        
-        String sql = "SELECT * FROM `documents-hash-bisnes_roles` WHERE idDocument=?";
-        
-        
-        class MapElementRowMapper implements RowMapper<MapElement>{
+        class MapElementRowMapper implements RowMapper<MapElement> {
+
             @Override
             public MapElement mapRow(ResultSet rs, int rowNum) throws SQLException {
                 return new MapElement(
-                        bisnesRoleDAO.getBisnesRoleById(rs.getInt("idBisnesRole"))
-                        ,rs.getString("annotation"));
+                        bisnesRoleDAO.getBisnesRoleById(rs.getInt("idBisnesRole")),
+                         rs.getString("annotation"));
             }
         }
-        List<MapElement> res= jdbcTemplate.query(sql, new MapElementRowMapper(),document.getId());
+        String sql = "SELECT * FROM `documents-hash-bisnes_roles` WHERE idDocument=?";
+
+        List<MapElement> res = jdbcTemplate.query(sql, new MapElementRowMapper(), document.getId());
         Map<BisnesRole, String> map = new HashMap<BisnesRole, String>(res.size());
-        for(MapElement el:res){
+        for (MapElement el : res) {
             map.put(el.br, el.annotation);
         }
 
-        
-        
         return map;
     }
-    
+
 }
 
 @Service
-class DocumentRowMapper implements RowMapper<DocumentLazy>{
+class DocumentRowMapper implements RowMapper<DocumentLazy> {
 
     @Override
     public DocumentLazy mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new DocumentLazy(rs.getInt("id")
-                ,rs.getString("name") 
-                ,rs.getString("annotation")
-                ,new Point(rs.getInt("positionX")
-                        , rs.getInt("positionY")) );
+        return new DocumentLazy(rs.getInt("id"),
+                 rs.getString("name"),
+                 rs.getString("annotation"),
+                 new Point(rs.getInt("positionX"),
+                         rs.getInt("positionY")));
     }
-    
+
 }
-
-
