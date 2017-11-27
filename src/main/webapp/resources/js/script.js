8828e5002a0e1ab72605495c4d055cb0f01b85d5
SergPF <sergeybakumenko.1996@gmail.com>
2017-11-27 20:56:05 +0200

    message icon changed, added autocpletion

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -5,25 +5,23 @@ $(document).ready(function () {
             $(this).parent().addClass("table-row-clicked");
         }
     });
-    
-        $("table tr").click(function () {
+
+    $("table tr").click(function () {
         document_role = $(this).find(".document-role").text();
         document_description = $(this).find(".document-description").text();
         $("#description_role").val(document_description);
         $("#document-role").attr("value", document_role);
-        console.log(document_role);
-        console.log(document_description);
     });
-    
-    $("table tr td a").click(function () {
-    if ($(this).attr("accountLink") !== undefined) {
-        currentAccountLink = $(this).attr("accountLink");
-        $("table tr:gt(0)").removeClass("table-row-clicked");
-        $("tr[accountNumber=" + currentAccountLink + "]").addClass("table-row-clicked");
-    }
-});
 
-$(".message-exit").click(function () {
-    $(this).parent().parent().css("display","none");
-});
+    $("table tr td a").click(function () {
+        if ($(this).attr("accountLink") !== undefined) {
+            currentAccountLink = $(this).attr("accountLink");
+            $("table tr:gt(0)").removeClass("table-row-clicked");
+            $("tr[accountNumber=" + currentAccountLink + "]").addClass("table-row-clicked");
+        }
+    });
+
+    $(".message-exit").click(function () {
+        $(this).parent().parent().css("display", "none");
+    });
 });
