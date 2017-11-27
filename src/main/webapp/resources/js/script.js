1539623b0a9299236d3546e178c91220f866cbf8
SergPF <sergeybakumenko.1996@gmail.com>
2017-11-27 23:39:27 +0200

    Added logic of editing document on the client side

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -6,12 +6,6 @@ $(document).ready(function () {
         }
     });
 
-    $("table tr").click(function () {
-        document_role = $(this).find(".document-role").text();
-        document_description = $(this).find(".document-description").text();
-        $("#description_role").val(document_description);
-        $("#document-role").attr("value", document_role);
-    });
 
     $("table tr td a").click(function () {
         if ($(this).attr("accountLink") !== undefined) {
@@ -23,5 +17,39 @@ $(document).ready(function () {
 
     $(".message-exit").click(function () {
         $(this).parent().parent().css("display", "none");
+    });
+
+//    Set standart action path to button in order to reset after clicking on the button
+    var standartRolesFormActionPath = $("#form_roles").attr("action");
+    function rulesButtonsToggle() {
+        $("#roleAddButton").toggleClass("invisible");
+        $("#roleEditButton").toggleClass("invisible");
+        $("#roleCancelButton").toggleClass("invisible");
+    }
+
+
+
+
+
+    $('a[pathUpdateCurrentRole]').click(function () {
+        var path = $(this).attr("pathUpdateCurrentRole");
+        $("#form_roles").attr("action", path);
+        rulesButtonsToggle();
+        document_role = $(this).parent().parent().find(".document-role").text();
+        document_description = $(this).parent().parent().find(".document-description").text();
+        $("#description_role").val(document_description);
+        $("#document-role").toggleClass("invisible");
+        $("#editDocumentRole").toggleClass("invisible");
+        $("#editDocumentRole").attr("value", document_role);
+    });
+
+    $("#roleCancelButton").click(function () {
+        $('#form_roles').attr("action", standartRolesFormActionPath);
+        $("#description_role").val("");
+        $("#document-role").attr("value", "");
+        $("#document-role").toggleClass("invisible");
+        $("#editDocumentRole").toggleClass("invisible");
+        rulesButtonsToggle();
+
     });
 });
